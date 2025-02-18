9ed043930e8a01778112d0e7f7ef7807b45d4cbb
Juliot Nuñez <nunezjuliot@gmail.com>
2025-02-18 03:27:32 -0400

    feat: Add mode field to User model
    Introduces a new 'mode' field to the User model to distinguish between client and specialist users. This includes:
    - Adding 'mode' to the User model's fillable attributes
    - Creating a migration to add a boolean 'mode' column to the users table
    - Setting a default value and comment to clarify the field's purpose

---
+++ database/migrations/2025_02_18_044948_add_mode_field_to_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('mode')->default(true)->comment('Indica el modo del usuario, si es true =  cliente de lo contrario es especialista');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('mode');
+        });
+    }
+};
